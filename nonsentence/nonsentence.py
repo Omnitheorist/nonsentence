e3e6e4b03603525a3b81422a6c85bc55d4fdd88e
Joel <joel@joel.tokyo>
2021-11-29 21:09:21 +1000

    refactor: don't redefine static consonant and vowel list every time

--- nonsentence/nonsentence.py
+++ nonsentence/nonsentence.py
@@ -2,11 +2,12 @@ import string
 import random
 
 
-def new_sentence(sentence_length, word_min_length, word_max_length):
-    # Create consonant and vowel list
-    vowels = [letter for letter in string.ascii_lowercase if letter in "aeiou"]
-    consonants = [letter for letter in string.ascii_lowercase if letter not in "aeiou"]
+# Create consonant and vowel list
+vowels = [letter for letter in string.ascii_lowercase if letter in "aeiou"]
+consonants = [letter for letter in string.ascii_lowercase if letter not in "aeiou"]
 
+
+def new_sentence(sentence_length, word_min_length, word_max_length):
     word = []
     sentence = []
 
